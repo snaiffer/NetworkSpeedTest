b946f43d2e29ef6882c309185eed27f6a5e1f093
Alexander <a.danilov@runabank.ru>
2020-07-13 13:06:05 +0300

    fixes

--- NetworkSpeedTest.sh
+++ NetworkSpeedTest.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-source /usr/lib/lib_bash_general/lib_bash_general.sh
+dest=$1 # copy test file to this server
+dest_port=$2
+fs=$3   # size of test file
 
-fs=100
-dest="snaiffer@192.168.1.34"
+# by default
+if [[ "$dest" = "" ]]; then dest="snaiffer@192.168.1.34"; fi
+if [[ "$dest_port" = "" ]]; then dest_port="22"; fi
+if [[ "$fs" = "" ]]; then fs=100; fi
+
 tf="/tmp/testfile.bin"
-tf_size="100M"
+tf_size=${fs}M
+
+source /usr/lib/bash/general.sh
 
 printf "Creating test file... "
 dd if=/dev/zero of=$tf bs=$tf_size count=1 &> /dev/null
 check_status
 
-echo "Establishing connection without password requiest... "
-ssh-keygen
-ssh-copy-id $dest
-check_status
-
 echo "Measuring network speed..."
 begint=`date +%s` && \
-scp $tf $dest:/tmp/ && \
+scp -P$dest_port $tf $dest:/tmp/ && \
 endt=`date +%s` && \
 speed=`echo "scale=1; $fs/($endt - $begint)" | bc -l`
-check_status
-
-printf "Tideing up... "
-rm -f $tf && \
-ssh $dest rm -f $tf
 check_status
 
 echo
